db1a6933afde37bf4a7ee1279fc8f4a47edd0fbc
Alex <alex@example.com>
2026-06-24 21:48:29 +0300

    finish2

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,25 +1,25 @@
 from app.restore_names import restore_names
 
 
-def test_restore_names_with_none_Jack() -> str:
+def test_restore_names_with_none_jack() -> str:
     users = [{"first_name": None, "full_name": "Jack Holy"}]
     restore_names(users)
     assert users[0]["first_name"] == "Jack"
 
 
-def test_restore_names_not_Jack() -> str:
+def test_restore_names_not_jack() -> str:
     users = [{"full_name": "Jack Holy"}]
     restore_names(users)
     assert users[0]["first_name"] == "Jack"
 
 
-def test_restore_name_none_Mike() -> str:
+def test_restore_name_none_mike() -> str:
     users = [{"first_name": None, "full_name": "Mike Adams"}]
     restore_names(users)
     assert users[0]["first_name"] == "Mike"
 
 
-def test_restore_name_not_Mike() -> str:
+def test_restore_name_not_mike() -> str:
     users = [{"full_name": "Mike Adams"}]
     restore_names(users)
     assert users[0]["first_name"] == "Mike"
