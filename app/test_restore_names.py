4984abb114848bf5ab8dbc3293ffe4c1503a1463
Alex <alex@example.com>
2026-06-24 21:45:00 +0300

    finish1

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,35 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names_with_none_Jack() -> str:
+    users = [{"first_name": None, "full_name": "Jack Holy"}]
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+
+
+def test_restore_names_not_Jack() -> str:
+    users = [{"full_name": "Jack Holy"}]
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+
+
+def test_restore_name_none_Mike() -> str:
+    users = [{"first_name": None, "full_name": "Mike Adams"}]
+    restore_names(users)
+    assert users[0]["first_name"] == "Mike"
+
+
+def test_restore_name_not_Mike() -> str:
+    users = [{"full_name": "Mike Adams"}]
+    restore_names(users)
+    assert users[0]["first_name"] == "Mike"
+
+
+def test_restore_names_with_none_or_not() -> str:
+    users = [
+        {"first_name": None, "full_name": "Jack Holy"},
+        {"full_name": "Mike Adams"}
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+    assert users[1]["first_name"] == "Mike"
